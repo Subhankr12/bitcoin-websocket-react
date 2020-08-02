59dbb859c2c84aab753c921bad40260dee09a53d
Subhankr12 <subhankr12@gmail.com>
2020-08-02 11:15:15 +0530

    Updates

---
+++ precache-manifest.db8eeaca23c17924c6b476c0e44351e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb267e3abc1031a85ec13e66e2ed4ca",
+    "url": "/bitcoin-websocket-react/index.html"
+  },
+  {
+    "revision": "a79db43e7edca681166c",
+    "url": "/bitcoin-websocket-react/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "25133f1eb55b08044ea9",
+    "url": "/bitcoin-websocket-react/static/js/2.1b0a955f.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/bitcoin-websocket-react/static/js/2.1b0a955f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79db43e7edca681166c",
+    "url": "/bitcoin-websocket-react/static/js/main.b1965a53.chunk.js"
+  },
+  {
+    "revision": "3bdd652a6aa7cc309329",
+    "url": "/bitcoin-websocket-react/static/js/runtime-main.e23437f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bitcoin-websocket-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
